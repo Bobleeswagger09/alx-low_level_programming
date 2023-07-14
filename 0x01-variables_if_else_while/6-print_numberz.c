4da308804626cabfd289a27a8af1b023b7737a40
Victor Samuel <samuelvictoralexander@gmail.com>
2023-07-14 14:58:35 -0700

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Print 0-9 and newline
+* Return: Always 0 (sucess)
+*/
+
+int main(void)
+{
+	int singleDigit;
+
+	for(singleDigit = 0; singleDigit < 10; singleDigit++);
+	{
+		putchar((singleDigit % 10) + '0');
+	}
+	putchar('\n');
+	return (0);
+}
