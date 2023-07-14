889e87d431970610d01428b52eae67413dcda182
Victor Samuel <samuelvictoralexander@gmail.com>
2023-07-14 13:04:41 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - display num 1-9
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+		printf("%d", num);
+	return (0);
+}
