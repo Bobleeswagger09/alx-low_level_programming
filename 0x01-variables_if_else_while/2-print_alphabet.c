af12644edad510a6247b3e355a38616cb4da60b0
Victor Samuel <samuelvictoralexander@gmail.com>
2023-07-14 10:46:31 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Print Alpha A-Z
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	char a[26] = "abcdefghijklmnopqrstuvwxyz";
+
+	int i;
+
+	for (i = 0; i < 26; i++)
+		putchar(a[i]);
+	putchar("\n");
+	return (0);
+}
+
