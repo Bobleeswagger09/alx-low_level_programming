98b1ed1068fd6e14621233ab825b9bcef8248346
Victor Samuel <samuelvictoralexander@gmail.com>
2023-07-14 15:19:16 -0700

    6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,11 +7,11 @@
 int main(void)
 {
 	int singleDigit;
-
-	for(singleDigit = 0; singleDigit < 10; singleDigit++)
+	for (singleDigit = 0; singleDigit < 10; singleDigit++)
 	{
 		putchar((singleDigit % 10) + '0');
 	}
 	putchar('\n');
+
 	return (0);
 }
