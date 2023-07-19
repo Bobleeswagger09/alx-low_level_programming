70ab6d6d4556dadff321fd5693a0d08b6d57184c
Victor Samuel <samuelvictoralexander@gmail.com>
2023-07-19 15:43:49 -0700

    100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* print_to_98 - prints the n times table, starting with 0
+* print_times_table - prints the n times table, starting with 0
 *
 * @n: prints from this number
 *
