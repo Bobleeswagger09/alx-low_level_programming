db43725dc26aaabb78fed53e8e62583c39c250d3
Victor Samuel <samuelvictoralexander@gmail.com>
2023-07-19 11:46:43 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,38 +8,37 @@
 
 void print_to_98(int n)
 {
-	int i, j, k;
+	int i, j;
+	int res;
 
 	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
-			for (j = 0; j <= n; j++)
+			_putchar('0');
+			for (j = 1; j <= n; j++)
 			{
-				k = j * i;
-				if (j == 0)
+				_putchar(',');
+				_putchar(' ');
+
+				res = i * j;
+				if (res == 99)
+					_putchar(' ');
+				if (res >= 9)
+					_putchar(' ');
+				if (res >= 100)
 				{
-					_putchar(k + '0');
+					_putchar((res / 100) + '0');
+					_putchar(((res / 10) % 10) + '0');
 				}
-				else if (k < 10 && j != 0)
+				else if (res <= 99 && res >= 10)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				}
-				else if (k >= 10 && k < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
+					_putchar((res / 10) + '0');
+					_putchar((res % 10) + '0');
+					_putchar('\n');
 				}
 			}
-			_putchar('\n');
 		}
+
 	}
 }
