bde1d449d8c1c6b115b6df3555e73a29d9c14afd
victor samuel <samuelvictoralexander@gmail.com>
2023-08-18 13:29:46 +0100

    2-print_strings.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 /**
 * print_numbers - a function that prints numbers, followed by a new line
-* @*separator: separator is the string to be printed between numbers
+* @separator: separator is the string to be printed between numbers
 * @n: n is the number of integers passed to the function
 */
 
@@ -13,14 +13,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list allnum;
 
 	va_start(allnum, n);
-
 	for (counter = 0; counter < n; counter++)
 	{
 		printf("%d", va_arg(allnum, unsigned int));
 		if (counter != (n - 1) && separator != (NULL))
-		{
 			printf("%s", separator);
-		}
 	}
 	printf("\n");
 	va_end(allnum);
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -4,6 +4,7 @@
 /**
 * print_strings - a function that prints numbers, followed by a new line.
 * @separator: space to be printed between the strings
+* @n: number of strings passed to the function
 * @...: number of augments to be printed
 */
 
@@ -12,18 +13,15 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int counter;
 	char *str;
 	va_list allstring;
-	
-	va_start(allstring, n);
 
+	va_start(allstring, n);
 	for (counter = 0; counter < n; counter++)
 	{
 		str = va_arg(allstring, char *);
-		
 		if (str == NULL)
 			printf("(nil");
 		printf("%s", str);
-
-		if (counter != (n -1) && separator != NULL)
+		if (counter != (n - 1) && separator != NULL)
 		{
 			printf("%s", separator);
 		}
