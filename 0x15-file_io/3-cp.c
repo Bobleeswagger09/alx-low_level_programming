54b04acc10852405b6a7258a24baaab67f218f40
Bobleeswagger09 <samuelvictoralexander@gmail.com>
2023-12-22 21:34:13 +0100

    i give my self away

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,97 +1,106 @@
 #include "main.h"
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 
 #define BUFFER_SIZE 1024
+char *buffer_normal(char *thefile);
+void close_normal(int thefile);
 
 /**
- * copy_and_closenormal - duplicates the content of a file
- * @FF: Source file normal
- * @SF: Destination file normal
- * Return: void
+ * buffer_normal - Allocates BUFFER_SIZE bytes to
+ * a buffer in memory.
+ * @thefile: naMe of file Buffer stores my cHars in.
+ *
+ * Return: pointer to a brand new buffer.
 */
 
-void copy_and_closenormal(int FF, int SF)
+char *buffer_normal(char *thefile)
 {
-	char buff[BUFFER_SIZE];
-	ssize_t theData;
+	char *thebuffer;
 
-	while ((theData = read(FF, buff, BUFFER_SIZE)) > 0)
+	thebuffer = malloc(sizeof(char) * BUFFER_SIZE);
+
+	if (thebuffer == NULL)
 	{
-		if (write(SF, buff, theData) != theData)
-		{
-			dprintf(STDERR_FILENO, "Error: Cannot write to file\n");
-			exit(99);
-		}
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", thefile);
+		exit(99);
 	}
 
-	if (close(FF) == -1 || close(SF) == -1)
+	return (thebuffer);
+}
+
+/**
+ * close_normal - I am here to close files.
+ * @thefile: the file to be closed.
+ */
+void close_normal(int thefile)
+{
+	int srcF;
+
+	srcF = close(thefile);
+
+	if (srcF == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close files\n");
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", srcF);
 		exit(100);
 	}
 }
 
 /**
- * copy_filenormal - duplicate Content of one file to another
- * @src: represents the path to the source
- * file that needs to be copied
- * @dest: represents the path to the destination normal
- * where content of source will be copied to.
- * Return: void. void! which means nothing
-*/
-
-void copy_filenormal(const char *src, const char *dest)
+ * main - i copy the contents of a file to another file.
+ * @argc: i am the number of aug that show up in program
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 meas success.
+ * Description: Ifi the argument count is incorrect - exit code 97.
+ * Ifi srcF does not exist or cannot be read now - exit code 98.
+ * Ifi destF cannot be created or written to destF - exit code 99.
+ * fi destF or file_from cannot be closed - exit code 100.
+ */
+int main(int argc, char *argv[])
 {
-	int FF, SF;
-	struct stat st;
+	char *bufferme;
+	int srcF, destF, rKage, wKage;
 
-	int dest_exist = stat(dest, &st);
-
-	FF = open(src, O_RDONLY);
-	if (FF == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
-		exit(98);
-	}
-	if (dest_exist == 0 && (st.st_mode & S_IFMT) == S_IFREG)
-		SF = open(dest, O_CREAT | O_WRONLY | O_TRUNC, st.st_mode);
-	else
-		SF = open(dest, O_CREAT | O_WRONLY | O_TRUNC, 0644);
-
-	if (SF == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Cannot open file %s\n", dest);
-		exit(99);
-	}
-
-	copy_and_closenormal(FF, SF);
-}
-
-/**
- * main - entry point of this program normal
- * @argc: rEpresents the number of cli augments
- * passed to the program when it's executed.
- * @argv: augment Vector; it an array of strings.
- * each string represents the one of the augments
- * passed to the program
- * Return: 0 onsuccess
-*/
-
-int main(int argc, char **argv)
-{
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	copy_filenormal(argv[1], argv[2]);
+
+	bufferme = buffer_normal(argv[2]);
+	srcF = open(argv[1], O_RDONLY);
+	rKage = read(srcF, bufferme, BUFFER_SIZE);
+	destF = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	do {
+		if (srcF == -1 || rKage == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
+			free(bufferme);
+			exit(98);
+		}
+
+		wKage = write(destF, bufferme, rKage);
+		if (destF == -1 || wKage == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
+			free(bufferme);
+			exit(99);
+		}
+
+		rKage = read(destF, bufferme, BUFFER_SIZE);
+		destF = open(argv[2], O_WRONLY | O_APPEND);
+
+	}
+	while (rKage > 0);
+	
+	free(bufferme);
+	close_normal(srcF);
+	close_normal(destF);
 
 	return (0);
 }
-
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,7 +10,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 ssize_t read_textfile(const char *filename, size_t letters);
 int _strlen(char *s);
 int create_file(const char *filename, char *text_content);
